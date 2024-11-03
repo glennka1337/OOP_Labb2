01a686cee56b625930442431a6e88d9ebc719dc4
glennka1337 <robloxfan03@gmail.com>
2024-11-03 15:25:38 +0100

    Add project files.

---
+++ OOP_Labb2/Animal.cs
@@ -0,0 +1,20 @@
+﻿namespace OOP_Labb2
+{
+    internal class Animal
+    {
+        public string Species { get; set; } = "TBD";
+        public int Legs { get; set; } = 4;
+        public int Eyes { get; set; } = 2;
+        public bool CanFly { get; set; } = false;
+        public bool IsExtinct { get; set; } = false;
+
+        public void OutputSpecies()
+        {
+            Console.WriteLine($"Species: {Species}");
+        }
+        public void PlaceHolder2()
+        {
+
+        }
+    }
+}
---
+++ OOP_Labb2/Björn.cs
@@ -0,0 +1,11 @@
+﻿namespace OOP_Labb2
+{
+    internal class Björn : Animal
+    {
+        public bool CanClimbTrees { get; set; } = true;
+        public void MakeSound()
+        {
+            Console.WriteLine("s");
+        }
+    }
+}
